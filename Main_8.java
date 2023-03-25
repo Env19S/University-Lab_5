b2a3d3d79405b27b032afd2d35e56e72e2f4a644
Env19S <117586607+Env19S@users.noreply.github.com>
2023-03-25 21:59:40 +0500

    Update Main_8.java

--- Main_8.java
+++ Main_8.java
@@ -1,18 +1,54 @@
+package org.example.task8;
+
 import java.util.Scanner;
 
-public class Main_8 {
+public class FastestAnimalsTask8 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите день недели ");
-        String a = in.nextLine();
-        switch (a){
-            case "Понедельник" -> System.out.println("Математика");
-            case "Вторник" -> System.out.println("Фмзкультура");
-            case "Среда" -> System.out.println("Информатика");
-            case "Четверг" -> System.out.println("Английский");
-            case "Пятница" -> System.out.println("Философия");
-            case "Суббота" -> System.out.println("Выходной");
-            case "Воскресенье" -> System.out.println("Выходной");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число от 1 до 8: ");
+        int rank = scanner.nextInt();
+
+        String animal;
+        int speed;
+
+        switch (rank) {
+            case 1:
+                animal = "Гепард";
+                speed = 75;
+                break;
+            case 2:
+                animal = "Антилопа";
+                speed = 61;
+                break;
+            case 3:
+                animal = "Лев";
+                speed = 50;
+                break;
+            case 4:
+                animal = "Койот";
+                speed = 43;
+                break;
+            case 5:
+                animal = "Страус";
+                speed = 43;
+                break;
+            case 6:
+                animal = "Заяц";
+                speed = 40;
+                break;
+            case 7:
+                animal = "Кенгуру";
+                speed = 40;
+                break;
+            case 8:
+                animal = "Черепаха";
+                speed = 30;
+                break;
+            default:
+                System.out.println("Неверное число. Введите число от 1 до 8.");
+                return;
         }
+
+        System.out.printf("Животное: %s, скорость: %d км/ч%n", animal, speed);
     }
 }
